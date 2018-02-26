6a660a0212191f120c1b222741b20d78983879cb
tuanzi <120362432@qq.com>
2018-02-26 17:08:46 +0800

    微信小程序

---
+++ readerMovie/pages/posts/post-detail/post-detail.js
@@ -0,0 +1,77 @@
+var postsData = require("../../../data/posts-data.js") 
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var id = options.id;
+    var postList = postsData.postlist
+    var _this = this
+    for (var i = 0; i < postList.length; i++){
+      if (id == postList[i].postId) {
+        console.log(postList[i]);
+        _this.setData({
+          postData: postList[i]
+        })
+      }
+    }
+    console.log(id);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
